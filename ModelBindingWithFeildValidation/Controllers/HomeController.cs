54788e0aef6c5f5cff1e64a001e2f9d38bd42b4b
Gauri Wadekar <gauriwadekar2417@gmail.com>
2024-05-08 07:44:47 +0530

    Add project files.

---
+++ ModelBindingWithFeildValidation/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Mvc;
+using ModelBindingWithFeildValidation.Models;
+public class HomeController : Controller
+{
+    public ActionResult EmployeeView()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public ActionResult SubmitForm(EmployeeValidation model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View("EmployeeView", model); // Return the view with validation errors
+        }
+        else
+        {
+           TempData["Name"] = model.Name;
+           return RedirectToAction("Success");
+        }
+    }
+
+    public string Success(EmployeeValidation model)
+    {
+        string name = TempData["Name"] as string;
+        // Clear the TempData to avoid storing unnecessary data
+        //TempData.Remove("Name");
+        return "welcome " + name;
+    }
+}
---
+++ ModelBindingWithFeildValidation/Models/EmployeeValidation.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModelBindingWithFeildValidation.Models
+{
+    public class EmployeeValidation
+    {
+        [Required(ErrorMessage = "Name is required!!")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required!!")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Mobile no is required!!")]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^(\d{10})$", ErrorMessage = "Wrong mobile")]
+        public string MobNo { get; set; }
+
+        [Required(ErrorMessage = "Designation is required!!")]
+        public string Designation { get; set; }
+
+
+    }
+}
